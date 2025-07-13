013cebc7eca2fee1311f8a090bc12383e0605951
Alan Diniz <alandiniz.dev@incibrasil.com.br>
2025-07-12 21:45:01 -0300

    🚧 Criação de Rqeust login e register

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($user);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso.',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciais inválidas.'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KpiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,5 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/kpis', [KpiController::class, 'index']);
